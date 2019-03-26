7887518a63cb1caeae303025080b37cb85000511
easonyes <2234148120@qq.com>
2019-03-26 22:28:27 +0800

    明天加油

--- js/myFunc.js
+++ js/myFunc.js
@@ -1,12 +1,17 @@
-/* 
-  返回传入 id 的标签
-*/
+/**
+ * 返回传入 id 的标签
+ * @param {string}id
+ * @returns {object}
+ */
 function $(id) {
   return typeof id === "string" ? document.getElementById(id) : null;
 }
 
-// 获取滚动的头部距离和左边距离
-// scroll().top , scroll().left
+/**
+ * 获取滚动的头部距离和左边距离
+ * scroll().top , scroll().left
+ * @returns {*}
+ */
 function scroll() {
   if(window.pageYOffset !== null) {
     return {
@@ -57,6 +62,98 @@ function stopPro() {
     event.stopPropagation();
   } else { // IE系列 IE 678
     event.cancelBubble = true;
+  }
+}
+
+/**
+ * 匀速动画函数
+ * @param {object}obj
+ * @param {number}target
+ * @param {number}speed
+ */
+function constant(obj, target, speed) {
+  // 1. 清除定时器
+  clearInterval(obj.timer);
+
+  // 2. 判断方向
+  let dir = obj.offsetLeft < target ? speed : -speed;
+
+  // 2. 设置定时器
+  obj.timer = setInterval(function () {
+    obj.style.left = obj.offsetLeft + dir + 'px';
+
+    if (Math.abs(target - obj.offsetLeft) < speed) {
+      clearInterval(obj.timer);
+      obj.style.left = target + 'px';
+    }
+  }, 50);
+}
+
+/**
+ * 缓动动画
+ * @param{object} obj
+ * @param data
+ * @param{number|缓动系数} n
+ * @param{number} ms
+ * @param{function} fn
+ */
+function buffer(obj, data, n, ms, fn) {
+// 1. 清除定时器
+  clearInterval(obj.timer);
+
+// 2. 设置定时器
+  let begin = 0, target = 0, speed = 0;
+  obj.timer = setInterval(function () {
+    // 2.0.0 旗帜
+    let flag = true;
+    for (let k in data) {
+      // 2.0 获取初始值
+      if ('opacity' === k) {
+        begin = Math.round(parseFloat(getStyleAttr(obj, k)) * 100 )  || 100;
+        target = parseInt(data[k] * 100);
+      } else {
+        begin = parseInt(getStyleAttr(obj, k)) || 0;
+        target = parseInt(data[k]);
+      }
+
+      // 2.1 求出步长
+      speed = (target - begin) / n;
+
+      // 2.2 判断取整方向
+      speed = (target > begin) ? Math.ceil(speed) : Math.floor(speed);
+      // 2.2 动起来
+      if ('opacity' === k) {
+        obj.style[k] = (begin + speed) / 100;
+        obj.style.filter = 'alpha(opacity:' + (begin + speed) + ')';
+      } else {
+        obj.style[k] = begin + speed + 'px';
+      }
+      if (begin !== target) {
+        flag = false;
+      }
+    }
+    // 3. 清除定时器
+    if (flag) {
+      clearInterval(obj.timer);
+
+      // 判断有没有回调函数
+      if (fn) {
+        fn();
+      }
+    }
+  }, ms);
+}
+/**
+ * 获取元素的css样式值
+ * @param{object} obj
+ * @param{string} attr
+ * @returns {*}
+ */
+function getStyleAttr(obj, attr) {
+  if (obj.currentStyle) { // IE 和 Opera
+    return obj.currentStyle[attr];
+  } else {
+    return window.getComputedStyle(obj, null)[attr];
   }
 }
 
